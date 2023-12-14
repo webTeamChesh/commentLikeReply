9ec753a28ddf46e0f951698d9c25b5552c1c1387
CEC WebTeam <webteam@cheshireeast.gov.uk>
2023-12-14 17:20:13 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 import { NodejsClient } from 'contensis-management-api/lib/client/nodejs-client.js';
 
+
 // Set some variables.
 const port = 3001;
 const ROOT_URL = `https://cms-${process.env.alias}.cloud.contensis.com/`;
